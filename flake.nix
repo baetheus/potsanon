16b39e6fafba54ab370fa0370640971089e156a4
Brandon Blaylock <brandon@null.pub>
2023-10-11 10:07:18 -0700

    feat: initial build

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A simple flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      shell = with pkgs; mkShell {
+        # Insert Packages Here
+        packages = [ deno ];
+      };
+    in {
+      devShells.default = shell;
+    });
+}
+
---
+++ routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main class="vw-p100 vhmn-vh100 fld-col ai-ctr jc-ctr flg-4">
+      <h1>Pots Anonymous</h1>
+      <h3>Coming Soon</h3>
+    </main>
+  );
+}
